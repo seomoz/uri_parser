21deb85524f50ca109e8fef18892bfd5f239c13f
Myron Marston <myron.marston@gmail.com>
2011-03-24 15:39:20 -0700

    Add some more specs.

--- spec/uri_parser_spec.rb
+++ spec/uri_parser_spec.rb
@@ -1,10 +1,19 @@
 require 'spec_helper'
 
 describe URIParser do
-  url = 'http://example.com/foo/bar?a=b&c=d'
+  def self.described_parsed(url, &block)
+    describe "URIParser.new('#{url}')" do
+      subject { described_class.new(url) }
+      module_eval(&block)
+    end
+  end
 
-  describe "URIParser.new('#{url}')" do
-    subject { described_class.new(url) }
+  described_parsed 'http://example.com/foo/bar?a=b&c=d' do
+    its(:scheme) { should == 'http' }
     its(:host) { should == 'example.com' }
+    its(:port) { should == '' }
+    its(:path) { should == '/foo/bar' }
+    its(:query) { should == 'a=b&c=d' }
+    its(:valid?) { should be_true }
   end
 end
