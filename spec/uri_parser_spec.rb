f1dd56e4ee09e71e95f4b2b64966c3fb28775569
Myron Marston <myron.marston@gmail.com>
2011-03-24 15:55:14 -0700

    Add more specs.

--- spec/uri_parser_spec.rb
+++ spec/uri_parser_spec.rb
@@ -1,19 +1,38 @@
+# -*- encoding: utf-8 -*-
 require 'spec_helper'
 
 describe URIParser do
-  def self.described_parsed(url, &block)
+  def self.describe_parsed(url, &block)
     describe "URIParser.new('#{url}')" do
       subject { described_class.new(url) }
       module_eval(&block)
     end
   end
 
-  described_parsed 'http://example.com/foo/bar?a=b&c=d' do
+  describe_parsed 'http://example.com/foo/bar?a=b&c=d' do
     its(:scheme) { should == 'http' }
     its(:host) { should == 'example.com' }
-    its(:port) { should == '' }
     its(:path) { should == '/foo/bar' }
     its(:query) { should == 'a=b&c=d' }
     its(:valid?) { should be_true }
+    its(:uri) { should == 'http://example.com/foo/bar?a=b&c=d' }
+  end
+
+  describe_parsed '@#4ioasfajdkshfas' do
+    its(:valid?) { should be_false }
+  end
+
+  describe_parsed 'http://руцентр.рф/Iñtërnâtiônàlizætiøn!?i18n=true' do
+    its(:valid?) { should be_true }
+    its(:scheme) { should == 'http' }
+    its(:host) { should == 'xn--e1aqhcjdv.xn--p1ai' }
+    its(:path) { should == '/I%C3%B1t%C3%ABrn%C3%A2ti%C3%B4n%C3%A0liz%C3%A6ti%C3%B8n!' }
+    its(:query) { should == 'i18n=true' }
+    its(:uri) { should == 'http://xn--e1aqhcjdv.xn--p1ai/I%C3%B1t%C3%ABrn%C3%A2ti%C3%B4n%C3%A0liz%C3%A6ti%C3%B8n!?i18n=true' }
+  end
+
+  describe_parsed 'http://subdomain.bar.com' do
+    its(:host) { should == 'subdomain.bar.com' }
+    its(:path) { should == '/' }
   end
 end
